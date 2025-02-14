af98475c8b431db1571059309b41d60bbc1aca0b
Anaru03 <dele22428@uvg.edu.gt>
2025-02-13 22:52:42 -0600

    Arcchivo ipynb

--- Formas.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-
-ggplot(datos, aes(x = genres)) +
-  geom_bar(fill = "skyblue") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(title = "Frecuencia de Géneros", x = "Género", y = "Cantidad")
